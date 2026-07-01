c5a7a798920b52b89a9de2d6779aa68f68b6c7af
Pranjal Kalal <pranjalrkalal29@gmail.com>
2026-07-01 16:25:55 +0530

    hs.v
    Added a testbench for the hs module to verify functionality.

---
+++ hs.v
@@ -0,0 +1,36 @@
+//design
+module hs(x,y,d,b);
+  input x,y;
+  output reg d,b;
+  always@(*)
+    assign d=x^y;
+ assign b=((~x)&y);
+endmodule
+
+//testbench
+module hstb();
+  reg x,y;
+  wire d,b;
+  hs dut(.x(x),.y(y),.d(d),.b(b));
+  initial 
+    begin
+      $dumpfile("dump.vcd");             
+      $dumpvars(0,hstb);
+    end
+  initial begin
+    x=0;
+    y=0;
+    #10
+    x=0;
+    y=1;
+    #20;
+    x=1;
+    y=0;
+    #30
+    x=1;
+    y=1;
+    #40
+    $finish;
+    end
+  
+endmodule
